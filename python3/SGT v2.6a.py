49c323abc1ad02233cf62de961add8af0154ae87
cmac4603 <colin@macrae.me>
2015-10-01 22:11:19 +0100

    ...

---
+++ python3/SGT v2.6a.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python3
+
+from datetime import date
+from openpyxl import load_workbook
+from ABE_ADCPi import ADCPi
+from ABE_helpers import ABEHelpers
+import os
+import time
+import RPi.GPIO as GPIO
+
+today = date.today()
+
+wb = load_workbook('SGT_Blank.xlsx')
+Pws = wb.get_sheet_by_name("Port")
+Sws = wb.get_sheet_by_name("Stbd")
+
+i2c_helper = ABEHelpers()
+bus = i2c_helper.get_smbus()
+adc = ADCPi(bus, 0x68, 0x69, 18)
+
+# Configure the GPIO pins
+BUTTON1 = 5
+BUTTON2 = 6
+BUTTON3 = 13
+BUTTON4 = 19
+EXIT_BUTTON = 23
+END_BUTTON = 18
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(BUTTON1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BUTTON2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BUTTON3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BUTTON4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(EXIT_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(END_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# voltage reading from channel 1 (v_supply), used for calccurrent()
+v = 3.3
+
+# calculates current from channel labelled 'v_i'
+# i = current calculated from adc channel 1 defined globally
+def calcCurrent(inval):
+    global i
+    i = ((inval) - 1.65) / 0.066
+    return ((inval) - 1.65) / 0.066
+    
+# calculates resistance using two global variables
+# voltage(V)/current(mA) = r
+def calcresistance():
+    return v / i
+
+def pre_exit(channel):
+    print('Exit button pressed, quiting program...')
+    print('Note, nothing saved...')
+    GPIO.cleanup()
+    time.sleep(1)
+    exit()
+
+def quit_program():
+    print('Quiting program...')
+    GPIO.cleanup()
+    time.sleep(1)
+    exit()
+
+
+def start():
+    os.system('clear')
+    print('Sleeve Gun Tester v2.1 2015')
+    print('Note you can press the red button anytime to quit')
+    GPIO.add_event_detect(EXIT_BUTTON, GPIO.FALLING, callback=pre_exit)
+    try:
+        # write resistance from S1 to cell
+        print('Attach banana plugs to firing line 1 for port guns')
+        print('Press green button to record resistance')
+        GPIO.wait_for_edge(BUTTON1, GPIO.FALLING)
+        calcCurrent(adc.read_voltage(1))
+        Pws['F37'] = calcresistance()
+        print('Solenoid resistance recorded')
+        time.sleep(1)
+        # write resistance from S2 to cell
+        print('Attach banana plugs to firing line 2 for port guns')
+        print('Press green button to record resistance')
+        GPIO.wait_for_edge(BUTTON2, GPIO.FALLING)
+        calcCurrent(adc.read_voltage(1))
+        Pws['F38'] = calcresistance()
+        print('Solenoid resistance recorded')
+        time.sleep(1)
+        # write resistance from S3 to cell
+        print('Attach banana plugs to firing line 3 for port guns')
+        print('Press green button to record resistance')
+        GPIO.wait_for_edge(BUTTON3, GPIO.FALLING)
+        calcCurrent(adc.read_voltage(1))
+        Pws['F39'] = calcresistance()
+        print('Solenoid resistance recorded')
+        time.sleep(1)
+        # write resistance from S4 to cell
+        print('Attach banana plugs to firing line 4 for port guns')
+        print('Press green button to record resistance')
+        GPIO.wait_for_edge(BUTTON4, GPIO.FALLING)
+        calcCurrent(adc.read_voltage(1))
+        Pws['F40'] = calcresistance()
+        print('Solenoid resistance recorded')
+        time.sleep(1)
+    finally:
+        print('Press the blue button to save to workbook & exit the program')
+        print('Or press the red button to just quit')
+        GPIO.wait_for_edge(END_BUTTON, GPIO.FALLING)
+        wb.save('SGT_rename.xlsx')
+        print('Workbook saved as SGT_rename.xlsx')
+        time.sleep(0.5)
+        quit_program()
+
+start()
